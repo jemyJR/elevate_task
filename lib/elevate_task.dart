09145b7c9d5162a826c58eef2c683a85bcae8232
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2024-09-17 23:36:46 +0300

    - Added screenutil for responsive ui

--- lib/elevate_task.dart
+++ lib/elevate_task.dart
@@ -1,6 +1,7 @@
 import 'package:elevate_task/core/routing/app_router.dart';
 import 'package:elevate_task/core/routing/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ElevateTask extends StatelessWidget {
   const ElevateTask({
@@ -11,11 +12,15 @@ class ElevateTask extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Elevate Task',
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: appRouter.generateRoute,
-      initialRoute: Routes.productsScreen,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'Elevate Task',
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: appRouter.generateRoute,
+        initialRoute: Routes.productsScreen,
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:elevate_task/core/routing/app_router.dart';
 import 'package:elevate_task/elevate_task.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
-  runApp(ElevateTask(
-    appRouter: AppRouter(),
-  ));
+void main() async {
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  await ScreenUtil.ensureScreenSize();
+
+  runApp(
+    ElevateTask(
+      appRouter: AppRouter(),
+    ),
+  );
 }
